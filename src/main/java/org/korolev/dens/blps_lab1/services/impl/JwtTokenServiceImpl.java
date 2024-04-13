cc0634290f29991320381d4de574a3ee9e1303e8
Denis Korolev <333165@niuitmo.ru>
2024-04-13 18:56:19 +0300

    fixes to allow working on remote server

--- src/main/java/org/korolev/dens/blps_lab1/services/impl/JwtTokenServiceImpl.java
+++ src/main/java/org/korolev/dens/blps_lab1/services/impl/JwtTokenServiceImpl.java
@@ -8,22 +8,25 @@ import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 import org.korolev.dens.blps_lab1.exceptions.InvalidTokenException;
 import org.korolev.dens.blps_lab1.services.JwtTokenService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
-import java.util.Properties;
 
 @Service
 public class JwtTokenServiceImpl implements JwtTokenService {
 
+    private final String secret;
+
+    public JwtTokenServiceImpl(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
     public String generateToken(Integer subjectId, String role) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + 86400000); // устанавливаем срок действия токена (1 день)
-
 
         return Jwts.builder()
                 .claim("role", role)
@@ -53,21 +56,8 @@ public class JwtTokenServiceImpl implements JwtTokenService {
     }
 
     private Key getSigningKey() {
-        byte[] keyBytes = getSecretProperty().getBytes(StandardCharsets.UTF_8);
+        byte[] keyBytes = this.secret.getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
-    }
-
-    private String getSecretProperty() {
-        Properties properties = new Properties();
-        String jwtSecret;
-        try {
-            FileInputStream inputStream = new FileInputStream("src/main/resources/client.properties");
-            properties.load(inputStream);
-            jwtSecret = properties.getProperty("jwt.secret");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return jwtSecret;
     }
 
 }
