9264ec62d5433a58fcd046b68500d5d4e810660a
Denis Korolev <333165@niuitmo.ru>
2024-09-15 05:11:42 +0300

    REST for Statistics service

--- NotificationService/src/main/java/com/denis/korolev/notificationservice/repositories/ClientRepository.java
+++ NotificationService/src/main/java/com/denis/korolev/notificationservice/repositories/ClientRepository.java
@@ -2,14 +2,7 @@ package com.denis.korolev.notificationservice.repositories;
 
 import com.denis.korolev.notificationservice.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ClientRepository extends JpaRepository<Client, String> {
-
-    @Modifying
-    @Query("UPDATE Client c SET c.rating = :newVal WHERE c.login = :clientId")
-    void updateRatingById(@Param("clientId") String clientId, @Param("newVal") Double newVal);
 
 }
--- NotificationService/src/main/java/com/denis/korolev/notificationservice/repositories/TopicRepository.java
+++ NotificationService/src/main/java/com/denis/korolev/notificationservice/repositories/TopicRepository.java
@@ -2,26 +2,7 @@ package com.denis.korolev.notificationservice.repositories;
 
 import com.denis.korolev.notificationservice.entities.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface TopicRepository extends JpaRepository<Topic, Integer> {
-
-    @Modifying
-    @Query("UPDATE Topic t SET t.views = :newVal WHERE t.id = :topicId")
-    void updateViewsById(@Param("topicId") Integer topicId, @Param("newVal") Integer newVal);
-
-    @Modifying
-    @Query("UPDATE Topic t SET t.fame = :newVal WHERE t.id = :topicId")
-    void updateFameById(@Param("topicId") Integer topicId, @Param("newVal") Integer newVal);
-
-    @Query("SELECT t FROM Topic t ORDER BY t.fame DESC LIMIT :N")
-    List<Topic> findTopNByFameDesc(@Param("N") Integer n);
-
-    @Query("SELECT t FROM Topic t ORDER BY t.views DESC LIMIT :N")
-    List<Topic> findTopNByViewsDesc(@Param("N") Integer n);
 
 }
